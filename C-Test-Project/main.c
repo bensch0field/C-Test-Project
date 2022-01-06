9c6ee6b533617d84e3291a43fc73597a2ce7b3e6
Ben Schofield <bensch0field@outlook.com>
2022-01-06 17:19:31 +0000

    first commit

---
+++ C-Test-Project/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  C-Test-Project
+//
+//  Created by Ben Schofield on 05/01/2022.
+//  Copyright © 2022 Ben Schofield. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("This is a test");
+    return 0;
+}
